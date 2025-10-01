0ab77f1366fced1f47512d1ab8b2f8e7ce21d366
Christian Ezeani <christian.ezeani@gmail.com>
2025-10-01 16:10:22 +0100

    Fix dbo test case

--- tests/models/person.go
+++ tests/models/person.go
@@ -5,10 +5,14 @@ import "gorm.io/gorm"
 type Person struct {
 	ID     uint64   `json:"id" gorm:"primaryKey;column:id;autoIncrement"`
 	Name   string   `gorm:"index;column:name"`
-	Cars   []*Car   `gorm:"foreignKey:OwnerID" gokit-dbo:"preload"`
-	Movies []*Movie `gorm:"foreignKey:OwnerID" gokit-dbo:"preload"`
+	Cars   []*Car   `gorm:"foreignKey:OwnerID"`
+	Movies []*Movie `gorm:"foreignKey:OwnerID"`
 }
 
-func (x Person) MoviesPreloader(db *gorm.DB) *gorm.DB {
+func (x Person) PreloadCars(db *gorm.DB) *gorm.DB {
+	return db
+}
+
+func (x Person) PreloadMovies(db *gorm.DB) *gorm.DB {
 	return db.Where("movie.isSeries = ?", false)
 }
